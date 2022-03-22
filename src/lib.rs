8b73974870564e11667d385851dd2e8efdea7f39
Lyssieth <lys@lys.ee>
2022-03-22 04:45:29 +0200

    more work

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,45 @@
 #![warn(clippy::pedantic)]
 
 use color_eyre::Report;
+use url::Url;
 
 pub type Res<T> = Result<T, Report>;
 
-#[must_use]
-pub fn do_stuff() -> bool {
-    println!("Doing...");
-    true
+pub struct App {
+    raw_lines: Vec<String>,
+    doc: Vec<String>,
+
+    max_x: isize,
+    max_y: isize,
+
+    current_bottom: isize,
+    current_top: isize,
+
+    url: Option<Url>,
+    path: String,
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn do_stuff_test() {
-        assert!(crate::do_stuff());
+impl App {
+    pub async fn new() -> Res<Self> {
+        let mut s = Self {
+            raw_lines: Vec::new(),
+            doc: Vec::new(),
+
+            max_x: -1,
+            max_y: -1,
+
+            current_bottom: -1,
+            current_top: -1,
+
+            url: None,
+            path: String::new(),
+        };
+
+        Ok(s)
+    }
+
+    pub fn with_url(mut self, url: &str) -> Res<Self> {
+        self.url = Some(url.parse()?);
+        Ok(self)
     }
 }
