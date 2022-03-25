13763e19505d49ef3568441784820f2462eb512e
Lyssieth <lys@lys.ee>
2022-03-25 10:45:40 +0200

    A basic working prototype.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #![warn(clippy::pedantic)]
 
-use std::{
-    io::{self, Stdout},
-    path::PathBuf,
-    time::Duration,
-};
+use std::path::PathBuf;
 
-use color_eyre::{eyre::bail, Report};
-use cursive::{views::TextView, Cursive, CursiveExt, CursiveRunnable};
+use color_eyre::Report;
+use cursive::{
+    align::{Align, HAlign},
+    theme::{BaseColor, BorderStyle, Color, Palette, PaletteColor, Theme},
+    traits::{Resizable, Scrollable},
+    view::Margins,
+    views::{LinearLayout, PaddedView, TextView, ThemedView},
+    Cursive, CursiveExt, CursiveRunnable,
+};
+use owo_colors::OwoColorize;
 
 type Res<T> = Result<T, Report>;
 
@@ -26,8 +30,42 @@ impl State {
 
     pub async fn run(&mut self) -> Res<()> {
         let siv = &mut self.cursive;
+        {
+            let theme = get_theme();
+            siv.set_theme(dbg!(theme));
+        }
 
-        siv.add_layer(TextView::new("Hello, world!"));
+        let size = siv.screen_size();
+
+        let main_content = TextView::new(lipsum::lipsum(2000))
+            .h_align(HAlign::Center)
+            .full_height();
+        let main_content = ThemedView::new(get_theme(), main_content);
+        let margins = Margins {
+            left: if size.x == 0 { 8 } else { size.x / 4 },
+            right: if size.x == 0 { 8 } else { size.x / 4 },
+            top: 0,
+            bottom: 0,
+        };
+
+        let layout = LinearLayout::vertical()
+            .child(
+                TextView::new("Title and Shit Like qThat")
+                    .center()
+                    .fixed_height(2),
+            )
+            .child(PaddedView::new(margins, main_content.scrollable()))
+            .child(
+                TextView::new(format!(
+                    "press {q} to quit, {s} to search, and {arrow_keys} to navigate",
+                    q = "q".yellow(),
+                    s = "s".yellow(),
+                    arrow_keys = "arrow keys".yellow()
+                ))
+                .align(Align::bot_right()),
+            );
+
+        siv.add_fullscreen_layer(layout);
 
         siv.add_global_callback('q', Cursive::quit);
 
@@ -43,17 +81,27 @@ async fn main() -> Res<()> {
 
     let mut state = State::new();
 
-    let path: PathBuf = "theme.toml".into();
-
-    if path.exists() {
-        let res = state.cursive.load_theme_file(path);
-
-        if let Err(err) = res {
-            bail!("{:?}", err);
-        }
-    }
-
     state.run().await?;
 
     Ok(())
+}
+
+fn get_theme() -> Theme {
+    let path: PathBuf = "theme.toml".into();
+
+    if path.exists() {
+        cursive::theme::load_theme_file(path).expect("failed to load theme from `theme.toml`")
+    } else {
+        let mut t = Theme {
+            shadow: false,
+            borders: BorderStyle::Simple,
+            palette: Palette::default(),
+        };
+
+        t.palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
+        t.palette[PaletteColor::View] = Color::Dark(BaseColor::Black);
+        t.palette[PaletteColor::Primary] = Color::Light(BaseColor::White);
+
+        t
+    }
 }
--- src/scrape.rs
+++ src/scrape.rs
@@ -1,9 +1,23 @@
+#![allow(unused)]
+
+use url::Url;
+
 use crate::Res;
 
-pub fn scrape_content(input: &str) -> Res<Output> {
-    todo!("implement scraping")
+pub fn search(input: &str) -> Res<Search> {
+    todo!("implement searching")
 }
 
+#[derive(Debug)]
+pub struct Search {
+    pub results: Vec<(Url, String)>,
+}
+
+pub fn load(url: &str) -> Res<Output> {
+    todo!("implement loading")
+}
+
+#[derive(Debug)]
 pub struct Output {
     pub title: String,
     pub content: String,
